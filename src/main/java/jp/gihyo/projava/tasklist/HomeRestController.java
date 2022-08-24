b01f8b977e1e41f77172ce6f6e68a4f6f7678ae7
syoho <syoho.xiaofan@gmail.com>
2022-08-24 17:08:57 +0800

    Modify. 1.タスクの情報を保持するためのモデルを作成する。 2.タスクを追加するエンドポイントを作成する。

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -2,12 +2,31 @@
 
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController//@RestControllerアノテーションはユーザーインタフェースとしてのHTMLを返さないWEBアプリケーションを作る場合に使われます
 public class HomeRestController{
+
+    record TaskItem(String id,String task,String deadline,boolean done){}
+    private List<TaskItem>taskItems = new ArrayList<>();
+
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task")String task,
+                   @RequestParam("deadline")String deadline){
+        String id = UUID.randomUUID().toString().substring(0,8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "タスクを追加しました！";
+    }
+
     @RequestMapping("/resthello")//リクエストを処理するメソッドであることを表す
     String hello(){
         return """
@@ -16,6 +35,8 @@ public class HomeRestController{
                 現在時刻は%sです。
                 """.formatted(LocalDateTime.now());
     }
+
+
 }
 
 
