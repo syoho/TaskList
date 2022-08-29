1c3f0d462d86d260f133a70b4e0c7a39c0d452b1
syoho <syoho.xiaofan@gmail.com>
2022-08-29 09:02:33 +0800

    Modify. 1.タスクを一覧表示するエンドポイントを実装する。 2.タスク登録のためのエンドポイントを実装する。

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -3,21 +3,51 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
 
-@RequestMapping(value="/hello")
+    @RequestMapping(value="/hello")
     String hello(Model model){
-    model.addAttribute("time",LocalDateTime.now());
-    return "hello";
-}
+        model.addAttribute("time",LocalDateTime.now());
+        return "hello";
+    }
 
-/*    @RequestMapping(value = "/hello")
+
+    //タスクを表すTaskItemレコードと、それを格納するためのtaskItemsフィールドを宣言する
+    record TaskItem(String id, String task,String deadline,boolean done){}
+    private List<TaskItem>taskItems = new ArrayList<>();
+
+
+    //タスクを一覧表示するエンドポイント
+    @GetMapping("/list")
+    String listItems(Model model){
+        model.addAttribute("tasklist",taskItems);
+        return "home";
+    }
+
+    //タスク登録のためのエンドポイント
+    @GetMapping("/add")
+    String addItem(@RequestParam("task")String task,
+        @RequestParam("deadline")String deadline){
+        String id = UUID.randomUUID().toString().substring(0,8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+        return "redirect:/list";
+    }
+
+
+
+/*@RequestMapping(value = "/hello")
     @ResponseBody
     String hello(){
         return """
