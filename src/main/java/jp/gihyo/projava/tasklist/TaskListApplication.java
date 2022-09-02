381d9a4bf2c64fb617caf8bd7eccf831594f5572
syoho <syoho.xiaofan@gmail.com>
2022-09-02 09:56:41 +0800

    Add and Modify. 1.CSSを使ってテンプレートを装飾する。

--- src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskListApplication {
+	public class TaskListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskListApplication.class, args);
